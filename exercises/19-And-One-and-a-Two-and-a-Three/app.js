f2f3ebb56b607e311843ebd95a66be8fa4cd11f8
mlanorero <mlanorero@gmail.com>
2020-08-08 14:24:49 +0000

    avanzando

--- exercises/19-And-One-and-a-Two-and-a-Three/app.js
+++ exercises/19-And-One-and-a-Two-and-a-Three/app.js
@@ -3,6 +3,12 @@ contact = {
     phone: "321-321-4321",
     email: "test@test.com"
 }
+
+contact.fullname = 'John Doe'
+contact.phone = '123-123-2134'
+contact.email = 'test@nowhere.com'
+
 for(key in contact){
     // Code goes here
+    console.log(key + ":" + " " + contact[key])
 }
