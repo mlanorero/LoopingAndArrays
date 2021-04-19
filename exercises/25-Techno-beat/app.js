642a5383f5cb1df3dcfe0bb0f8e28c214ac5accf
mlanorero <mlanorero@gmail.com>
2021-04-19 23:29:20 +0000

    a uno de terminar!

--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,18 +1,20 @@
-let string = '';
-
-function lyricsGenerator (arr){
-    for(let i = 0; i < arr.lenght; i++){
-        if(arr[i] === 0){
-            string = 'Boom';
-        } else if( arr[i] === 1){
-            string = 'Drop the base';
-        } 
+function lyricsGenerator(arr) {
+    let string = "";
+    for (let i = 0; i < arr.lenght; i++) {
+        if (arr[i] === 0) {
+            string += "Boom ";
+        } else if (arr[i] === 1) {
+            string += "Drop the base ";
+            if(arr[i]+1  === 1 && arr[i] +2 === 1){
+                string += '!!!Break the base!!! ';
+            }
+        }
     }
-    return array;
+    return string;
 }
 // test Data
-console.log(lyricsGenerator([0,0,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,1,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,0]))
-console.log(lyricsGenerator([1,0,1]))
-console.log(lyricsGenerator([1,1,1]))
+console.log(lyricsGenerator([0, 0, 1, 1, 0, 0, 0]))
+console.log(lyricsGenerator([0, 0, 1, 1, 1, 0, 0, 0]))
+console.log(lyricsGenerator([0, 0, 0]))
+console.log(lyricsGenerator([1, 0, 1]))
+console.log(lyricsGenerator([1, 1, 1]))
